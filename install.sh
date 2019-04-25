7f8567272c683d2adeaa99faab55f2c9b0e94e61
NoharaMasato <masato338914914@gmail.com>
2019-04-25 13:25:45 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-ln -s ~/dotfiles/.bash_profile ~/
-ln -s ~/dotfiles/.bashrc ~/
-ln -s ~/dotfiles/.vimrc ~/
+# ln -s ~/dotfiles/.bash_profile ~/
+# ln -s ~/dotfiles/.bashrc ~/
+# ln -s ~/dotfiles/.vimrc ~/
+
+DOTPATH=~/.dotfiles
+
+for f in .??*
+do
+  [ "$f" = ".git" ] && continue
+  ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+done
