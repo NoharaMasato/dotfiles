c2158a20da46e6af6d1e5f9231754b26e236fd8e
NoharaMasato <masato338914914@gmail.com>
2019-10-05 18:32:53 +0900

    fix

--- .bash/.prompt.bash
+++ .bash/.prompt.bash
@@ -1,8 +1,17 @@
 disc_ratio=`df -h | sed -n 2P | awk '{print $5}'`
 echo "ディスクの使用量は${disc_ratio}です"
 
+case "${OSTYPE}" in
+  darwin*)
+    DOTFILE_HOME=/Users/noharamasato2
+    ;;
+  linux*)
+    DOTFILE_HOME=/home/nohara
+    ;;
+esac
+
 # git補間とプロンプト表示にブランチ名を出すかつ、短くする
-source /Users/noharamasato2/.git-completion.bash
-source /Users/noharamasato2/.git-prompt.sh
+source $DOTFILE_HOME/.git-completion.bash
+source $DOTFILE_HOME/.git-prompt.sh
 export PS1='\[\033[35m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
 GIT_PS1_SHOWDIRTYSTATE=true
--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-# ln -s ~/dotfiles/.bash_profile ~/
-# ln -s ~/dotfiles/.bash/ ~/
-# ln -s ~/dotfiles/.vimrc ~/
-# ln -s ~/dotfiles/.tmux.conf ~/
-# ln -s ~/dotfiles/.gitconfig ~/
-
 DOTPATH=~/dotfiles
 
 for f in .??*
