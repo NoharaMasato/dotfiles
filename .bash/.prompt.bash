f7eb2c6eef1d7d1fcd5292a18bf8d150571cbebc
NoharaMasato <masato338914914@gmail.com>
2019-10-06 15:42:31 +0900

    linuxではpromptにホスト名を表示するように変更

--- .bash/.prompt.bash
+++ .bash/.prompt.bash
@@ -1,17 +1,21 @@
 disc_ratio=`df -h | sed -n 2P | awk '{print $5}'`
 echo "ディスクの使用量は${disc_ratio}です"
 
+function git_prompt(){
+  # git補間とプロンプト表示にブランチ名を出すかつ、短くする
+  source $1/.git-completion.bash
+  source $1/.git-prompt.sh
+  GIT_PS1_SHOWDIRTYSTATE=true
+}
+
 case "${OSTYPE}" in
   darwin*)
-    DOTFILE_HOME=/Users/noharamasato2
+    git_prompt /Users/noharamasato2
+    export PS1='\[\033[35m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
     ;;
   linux*)
-    DOTFILE_HOME=/home/nohara
+    git_prompt /home/nohara
+    export PS1='\[\033[35m\]\h:\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
     ;;
 esac
 
-# git補間とプロンプト表示にブランチ名を出すかつ、短くする
-source $DOTFILE_HOME/.git-completion.bash
-source $DOTFILE_HOME/.git-prompt.sh
-export PS1='\[\033[35m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
-GIT_PS1_SHOWDIRTYSTATE=true
