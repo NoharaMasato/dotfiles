248fe4f577ea44885d284f17e8ba3a4ae1ff3039
NoharaMasato <masato338914914@gmail.com>
2021-04-26 11:23:43 +0900

    edit

--- .bash/.aliases.bash
+++ .bash/.aliases.bash
@@ -8,7 +8,7 @@ function cp_to_clip_bload(){
   cat $1 | pbcopy
 } 
 
-function compile_and_run_c(){
+function compile_and_run_cpp(){
   g++ -Wall -Wextra -o out $1 && ./out
 }
 
@@ -18,7 +18,7 @@ function copy_atcoder_template_master(){
     echo "実行するには1個の引数が必要です。" 1>&2
     return 1
   fi
-  cp -n /Users/noharamasato2/Desktop/atcoder/template/master.cpp ./$1.cpp
+  cp -n $HOME/Desktop/atcoder/template/master.cpp ./$1.cpp
 }
 
 function make_all_atcoder_files(){
@@ -26,9 +26,6 @@ function make_all_atcoder_files(){
 	do
     copy_atcoder_template_master $x
 	done
-  #cp /Users/noharamasato2/Desktop/atcoder/template/x.sh ./
-  #/Users/noharamasato2/Desktop/atcoder/template/init.sh # init directory
-  #go build /Users/noharamasato2/Desktop/atcoder/template/fetch_testcase.go
 }
 
 function go_back(){ # if command start from .. , it is "cd" command
@@ -39,28 +36,24 @@ alias '..'=go_back
 
 alias du='du -s -m -c * | sort -n -r' # -m:MB,-c:caliculate sum
 alias rm='rm -i' # confirm whem remove
-alias de='cd ~/Desktop'
 alias hi='history'
-alias l='ls'
-
-alias dc='docker'
-alias dcc='docker-compose'
-alias k='kubectl'
 
 case "${OSTYPE}" in
   darwin*) # mac
-    alias less='/usr/share/vim/vim81/macros/less.sh' # less をvimの支配下にあるlessを使う
+    alias less='/usr/share/vim/vim82/macros/less.sh' # less をvimの支配下にあるlessを使う
     alias ls="ls -G"
     alias la="ls -aG" 
     alias ll="ls -lG"
     alias lal="ls -laG"
     alias lla="ls -laG"
-    alias "rails_dk"="docker-compose run web bin/rails"
-    alias "rails_dks"="docker-compose run --rm --service-ports web"
+
+    alias westzoom="open $WEST_ZOOM_URL"
+
+    alias norm="git ls-files -x *h *c | xargs norminette"
 
     #atcoder
     alias cpc=cp_to_clip_bload
-    alias ga=compile_and_run_c
+    alias ga=compile_and_run_cpp
     alias cpt=copy_atcoder_template_master #指定したアルファベットの.cppファイルをmasterから作成
     alias acp=make_all_atcoder_files
     ;;
--- .bash_profile
+++ .bash_profile
@@ -1,9 +1,10 @@
 bash_conf=~/.bash
 
-# load all files inside bash_conf
 for file in $(ls -aF $bash_conf/ | grep -v /)
 do
   [ "$file" = ".bashrc" ] && continue
   . $bash_conf/$file
 done
+
+source ~/.env
 
